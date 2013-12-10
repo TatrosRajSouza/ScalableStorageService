504d8609f40d00192be6a9d210068a6b0c9bc143
Claynon Souza <claynon@gmail.com>
2013-12-10 22:18:21 +0100

    Solving problem of delay of ssh starting the server node

--- src/app_kvEcs/ECS.java
+++ src/app_kvEcs/ECS.java
@@ -41,7 +41,8 @@ public class ECS {
 	public void addNode() {
 		String address;
 		int port;
-		
+		KVCommunication comm;
+
 		ServerData node = moveRandomNode(serverRepository, storageService);
 		if (node == null) {
 			//TODO exception or something like that
@@ -52,16 +53,21 @@ public class ECS {
 		}
 		hashing.addServer(address, port);
 		runScript(address, port);
-		try {
-			KVCommunication comm = new KVCommunication(address, port);
+		//try {
+			//Thread.sleep(500);
+			comm = connectServerNode(address, port);
 			communications.put(address + ":" + Integer.toString(port) , comm);
-		} catch (UnknownHostException e) {
+			comm.closeConnection();
+		/*} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 		//Initialize the new storage server with the updated meta­data and start it.
 		//Set write lock (lockWrite()) on the successor node;
 		//Invoke the transfer of the affected data items to the new storage server:
@@ -70,6 +76,8 @@ public class ECS {
 		//	Send a meta­data update to all storage servers
 		//	Release the write lock on the successor node and finally remove the data items
 		//	that are no longer handled by this server
+
+
 	}
 
 	public void removeNode() {
@@ -77,7 +85,7 @@ public class ECS {
 		hashing.update(storageService.getServers());
 		//TODO add a removeServer() na InfrastructureMetadata e substituir esse update
 	}
-	
+
 	public void start() {
 		for (ServerData node : storageService.getServers()) {
 			startNode(node);
@@ -87,9 +95,9 @@ public class ECS {
 
 	private void startNode(ServerData node) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public void stop() {
 		for (ServerData node : storageService.getServers()) {
 			stopNode(node);
@@ -99,7 +107,7 @@ public class ECS {
 
 	private void stopNode(ServerData node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void defineServerRepository(String fileName) throws Exception {
@@ -120,7 +128,8 @@ public class ECS {
 		br.close();
 	}
 
-	private void runScript(String address, int port) {
+	//private
+	public void runScript(String address, int port) {
 		//Process proc;
 		String[] args = {"./script.sh", address, Integer.toString(port)};
 
@@ -143,7 +152,7 @@ public class ECS {
 	public InfrastructureMetadata getStorageService() {
 		return storageService;
 	}
-	
+
 	public ConsistentHashing getHashing() {
 		return hashing;
 	}
@@ -163,10 +172,21 @@ public class ECS {
 
 		randomIndex = generator.nextInt(servers.size());
 		node = servers.get(randomIndex);
-		
+
 		to.addServer(node.getName(), node.getAddress(), node.getPort());
 		from.removeServer(node.getAddress(), node.getPort());
 		return node;
+	}
+	
+	private KVCommunication connectServerNode(String address, int port) {
+		//solves problem of the delay of initializing the nodes with a ssh connection
+		try {
+			return new KVCommunication(address, port);
+		} catch (UnknownHostException e) {
+			return connectServerNode(address, port);
+		} catch (IOException e) {
+			return connectServerNode(address, port);
+		}
 	}
 
 }
--- src/app_kvEcs/ECSClient.java
+++ src/app_kvEcs/ECSClient.java
@@ -1,10 +1,19 @@
 package app_kvEcs;
 
-
 public class ECSClient {
 	static ECS ecs = new ECS();
 	public static void main(String args[]) {
 		System.setProperty("file.encoding", "US-ASCII");
-		//ecs.runScript("127.0.0.1", 50001);
+		try {
+			ecs.defineServerRepository("./ecs.config");
+			ecs.initService(8);
+			/*ecs.runScript("127.0.0.1", 50000);
+			Thread.sleep(500);
+			new KVCommunication("127.0.0.1", 50000);*/
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
