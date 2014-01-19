fa27900db540890615c01670f492e49008a3dcf4
claynon <claynon@gmail.com>
2014-01-19 15:23:49 +0100

    Fixing and adding the implementation

--- src/common/messages/ServerServerMessage.java
+++ src/common/messages/ServerServerMessage.java
@@ -7,6 +7,8 @@ public class ServerServerMessage {
 	private String key;
 	private String value;
 	private KVData serverData;
+	private int numServer;
+	private final String EMPTY = "EMPTY";
 
 	public ServerServerMessage(byte[] bytes) throws InvalidMessageException {
 		String message;
@@ -21,59 +23,82 @@ public class ServerServerMessage {
 
 		if (bytes.length <= KVMessage.DROP_SIZE) {
 			setType(arguments[0]);
+			numServer = Integer.parseInt(arguments[1]);
 			checkLength(arguments.length);
 			switch (command) {
 			case SERVER_PUT_ALL:
-				//TODO figure out how to discover which server is sending the message
-				//TODO implement this constructor
-				serverData = new KVData(arguments[2]);
+				if (arguments[2].equals(EMPTY)) {
+					serverData = new KVData();
+				} else {
+					serverData = new KVData(arguments[2]);
+				}
 				break;
 			case SERVER_PUT:
-				key = arguments[1];
-				value = arguments[2];
+				key = arguments[2];
+				value = arguments[3];
 				break;
 			case SERVER_DELETE:
-				key = arguments[1];
+				key = arguments[2];
 				break;
 			}
 		}
 	}
-	
-	public ServerServerMessage(ServerServerStatustype command, String key) {
+
+	public ServerServerMessage(ServerServerStatustype command, int numServer, String key) {
 		if (command != ServerServerStatustype.SERVER_DELETE) {
 			//throw
 		}
 		this.command = command;
+		this.numServer = numServer;
 		this.key = key;
-		this.value = null;
+		value = null;
 		serverData = null;
+		numServer = 0;
 	}
-	
-	public ServerServerMessage(ServerServerStatustype command, String key, String value) {
+
+	public ServerServerMessage(ServerServerStatustype command, int numServer, String key, String value) {
 		if (command != ServerServerStatustype.SERVER_PUT) {
 			//throw
 		}
 		this.command = command;
+		this.numServer = numServer;
 		this.key = key;
 		this.value = value;
 		serverData = null;
+		numServer = 0;
+	}
+
+	public ServerServerMessage(ServerServerStatustype command, int numServer, KVData serverData) {
+		if (command != ServerServerStatustype.SERVER_PUT_ALL) {
+			//throw
+		}
+		this.command = command;
+		this.numServer = numServer;
+		this.serverData = serverData;
+		key = null;
+		value = null;
 	}
 
 	public byte[] toBytes() {
 		byte[] bytes = null;
-		String message = command.toString() + "\n";
+		String message = command.toString() + "\n" + numServer + "\n";
 
 		switch (command) {
 		case SERVER_DELETE:
-			message += key + "\n\r";
+			message += key + "\r";
 			break;
 		case SERVER_PUT:
-			message += key + "\n" + value + "\n\r";
+			message += key + "\n" + value + "\r";
 			break;
 		case SERVER_PUT_ALL:
+			if (serverData.toString().length() != 0) {
+				message += serverData.toString() + "\r";
+			} else {
+				message += EMPTY + "\r";
+			}
 			break;
 		}
-		
+
 		bytes = message.getBytes();
 		/*if (bytes.length > KVMessage.DROP_SIZE) {
 			logger.error("Cannot convert KVQuery to bytes, since the payload would be too large.\n"
@@ -84,17 +109,17 @@ public class ServerServerMessage {
 
 		return bytes;
 	}
-	
+
 	private void checkLength(int length) throws InvalidMessageException {
 		switch (command) {
-		case SERVER_PUT_ALL:
 		case SERVER_PUT:
-			if (length != 3) {
+			if (length != 4) {
 				throw new InvalidMessageException("Incorrect number of arguments");
 			}
 			break;
+		case SERVER_PUT_ALL:
 		case SERVER_DELETE:
-			if (length != 2) {
+			if (length != 3) {
 				throw new InvalidMessageException("Incorrect number of arguments");
 			}
 			break;
@@ -114,11 +139,19 @@ public class ServerServerMessage {
 		return value;
 	}
 
+	public int getNumServer() {
+		return numServer;
+	}
+
 	private void setType(String command) throws InvalidMessageException {
 		try {
 			this.command = ServerServerStatustype.valueOf(command);
 		} catch (Exception ex) {
 			throw new InvalidMessageException("This code does not represent a command.");        
 		}
+	}
+
+	public KVData getData() {
+		return serverData;
 	}
 }
