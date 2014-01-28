a1cf08312270162569290c64680bbb1d4d79d134
claynon <claynon@gmail.com>
2014-01-28 09:05:06 +0100

    Turning debug back on

--- src/app_kvClient/KVClient.java
+++ src/app_kvClient/KVClient.java
@@ -22,7 +22,7 @@ import client.KVStore;
  * @author Elias Tatros
  */
 public class KVClient {
-	public static final boolean DEBUG = false;
+	public static final boolean DEBUG = true;
 	private Logger logger;
 	// private static Logger rootLogger = Logger.getRootLogger();
 	private KVStore kvStore = null;
